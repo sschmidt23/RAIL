4865eb0a6214d37315aa28823afaffaae702ed21
Alex Malz <aimalz@nyu.edu>
2020-02-19 17:04:01 +0100

    skeleton for estimator

---
+++ rail/estimation/estimator.py
@@ -0,0 +1,78 @@
+"""
+A superclass for estimators of redshift posterior PDFs
+"""
+
+from __future__ import absolute_import
+__all__ = ['Estimator']
+
+import qp
+import rail
+from rail.formats import Public
+from rail.formats import Prior
+from rail.formats import Data
+from rail.formats import Result
+
+
+class Estimator(object):
+
+    def __init__(self, name=None):
+        """
+        An object that constrains redshifts of a sample of galaxies
+
+        Parameters
+        ----------
+        name: string
+            the name of the estimation routine
+
+        Notes
+        -----
+        This is not the wrapper for redshift estimation codes!
+        The wrapped codes, however, should be subclasses of the rail.estimator.Estimator class.
+        """
+        self.name = name
+
+    def prepare(self, public_info, **kwds):
+        """
+        Trains and/or tunes prior information based on available external information
+
+        Parameters
+        ----------
+        public_info: rail.formats.Public object
+            all external information available to the rail.estimator.Estimator object
+
+        Returns
+        -------
+        prior_info: rail.formats.Prior object
+            information necessary for Estimator.estimate(), derived from external_info
+
+        Notes
+        -----
+        Since the format of prior_info is presumably unique to each estimator, it contains whatever information the estimator expects.
+        The rail-specific format would just provide some bookkeeping functionality for us as experimenters.
+        It is intentional that the rail.estimator.Estimator class does not carry around training inputs/outputs, to keep it lightweight and prevent spillover between experiments.
+        """
+        prior_info = rail.formats.Prior()
+        return(prior_info)
+
+    def estimate(self, data, prior_info, **kwds):
+        """
+        Estimates redshift posterior PDFs of a galaxy sample with given data and requisite prior information
+
+        Parameters
+        ----------
+        data: rail.formats.Data object
+            all available data for a set of galaxies with unknown redshifts
+        prior_info: rail.formats.Prior object
+            prior information required for redshift posterior PDF estimation as processed from available external information by
+
+        Returns
+        -------
+        estimates: rail.formats.Result object
+            redshift posterior PDFs for the input data set
+
+        Notes
+        -----
+        It might make sense for rail.formats.Result to actually be a qp.ensemble.Ensemble object (or an improved future version thereof) so the estimated redshift posteriors can remain in their native format and be easily convertible without reinventing the wheel on the metrics I/O.
+        """
+        estimates = rail.formats.Result()
+        return(estimates)
