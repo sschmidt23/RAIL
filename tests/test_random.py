bc5486a001f419e2ef0c6245a0ed79415e5407a3
sschmidt23 <sschmidt@physics.ucdavis.edu>
2020-09-22 21:52:45 -0700

    add sklearn test

--- tests/test_random.py
+++ tests/test_random.py
@@ -3,7 +3,7 @@ from rail.estimation.estimator import Estimator
 from rail.estimation.utils import *
 # this is temporary until unit test uses a definite test data set and creates
 # the file on the fly
-from rail.estimation.algos import randomPZ
+from rail.estimation.algos import randomPZ, flexzboost, sklearn_nn
 
 test_base_yaml = './tests/base.yaml'
 
@@ -23,3 +23,18 @@ def test_random():
     assert end == pz.num_rows
     xinputs = inputs['run_params']
     assert len(pz.zgrid) == np.int32(xinputs['nzbins'])
+
+def test_simpleNN():
+    """
+    A basic test of simpleNN subclass
+    """
+    inputs = {'run_params': {'width': 0.025, 'zmin': 0.0, 'zmax': 3.0, 
+                             'nzbins': 301}}
+
+    pz=sklearn_nn.simpleNN(test_base_yaml, inputs)
+    for _, end, data in iter_chunk_hdf5_data(pz.testfile, pz._chunk_size,
+                                             pz.hdf5_groupname):
+        pz_dict = pz.estimate(data)
+        assert end == pz.num_rows
+        xinputs = inputs['run_params']
+        assert len(pz.zgrid) == np.int32(xinputs['nzbins'])
