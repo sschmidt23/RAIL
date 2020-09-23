4068c9e9da1ec14f211b6795c256108690802faa
sschmidt23 <sschmidt@physics.ucdavis.edu>
2020-09-22 22:55:45 -0700

    general test for all 3 codes

--- tests/test_algos.py
+++ tests/test_algos.py
@@ -7,12 +7,12 @@ from rail.estimation.algos import randomPZ, sklearn_nn, flexzboost
 
 test_base_yaml = './tests/base.yaml'
 
-specific_estimator=[randomPZ.randomPZ, sklearn_nn.simpleNN]
+specific_estimator=[sklearn_nn.simpleNN, randomPZ.randomPZ, flexzboost.FZBoost]
 input_dicts = [
     {'run_params': {'width': 0.025, 'zmin': 0.0, 'zmax': 3.0,
                     'nzbins': 301}}, 
     {'run_params': {'rand_width': 0.025, 'rand_zmin': 0.0, 'rand_zmax': 3.0,
-                    'nzbins': 301}}
+                    'nzbins': 301}},
     {'run_params': {'zmin': 0.0, 'zmax': 3.0, 'nzbins': 301, 'trainfrac': 0.75,
                     'bumpmin': 0.02, 'bumpmax': 0.35, 'nbump': 20, 'sharpmin': 0.7,
                     'sharpmax': 2.1, 'nsharp': 15, 'max_basis': 35, 
@@ -23,7 +23,7 @@ input_dicts = [
     ]
 
  
-def test_algo(specific_estimator):
+def test_algo():
     """
     A basic test of simpleNN subclass
     """
@@ -35,5 +35,5 @@ def test_algo(specific_estimator):
                                                  pz.hdf5_groupname):
             pz_dict = pz.estimate(data)
         assert end == pz.num_rows
-        xinputs = inputs['run_params']
+        xinputs = one_input['run_params']
         assert len(pz.zgrid) == np.int32(xinputs['nzbins'])
